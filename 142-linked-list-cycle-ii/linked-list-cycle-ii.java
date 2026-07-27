ffd6ba372189924e8f5cbd4ab39e58299255b0ae
Divyansh Dubey <167629510+divyanshh19@users.noreply.github.com>
2026-07-28 00:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (87.35%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,29 +11,38 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        // ListNode slow = head;
-        // ListNode fast = head;
+        //Brute force using hashMap technique
 
-        // while(fast != null && fast.next != null){
-        //     slow = slow.next;
-        //     fast = fast.next.next;
-
-        //     if(slow == fast){
-        //         return slow;
-        //     }
+        // if(head == null || head.next ==  null){
+        //     return null;
         // }
+        // HashMap<ListNode , Integer> map = new HashMap<>();
+        // ListNode temp = head;
+        // while(temp != null){
+        //     if(map.containsKey(temp)){
+        //         return temp;
+        //     }
+        //     map.put(temp , 1);
+        //     temp = temp.next;
+        // }
+        // return null;
+        //optimal approach using tortoise hare
+        ListNode slow = head;
+        ListNode fast = head;
 
-        if(head == null || head.next ==  null){
-            return null;
-        }
-        HashMap<ListNode , Integer> map = new HashMap<>();
-        ListNode temp = head;
-        while(temp != null){
-            if(map.containsKey(temp)){
-                return temp;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                slow = head;
+                
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
-            map.put(temp , 1);
-            temp = temp.next;
         }
         return null;
     }
