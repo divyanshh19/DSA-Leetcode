ece32b6afe275a69eab2b638ef326fa096e065b0
Divyansh Dubey <167629510+divyanshh19@users.noreply.github.com>
2026-07-31 00:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (63.65%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -33,8 +33,11 @@ class Solution {
         int i = 0;
         for(int j = 1;j < nums.length;j++){
             if(nums[j] != nums[i]){
-                nums[i+1] = nums[j];    //take the front position i+1
+                // nums[i+1] = nums[j];    //take the front position i+1
+                // i++;
+                //or first go to next position then update it
                 i++;
+                nums[i] = nums[j];
             }
         }
         return (i + 1); //as we have to tell the total number of elements
