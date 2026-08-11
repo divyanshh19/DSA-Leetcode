4f976c7d5683a2e83c74fa413c5428d092687389
Divyansh Dubey <167629510+divyanshh19@users.noreply.github.com>
2026-08-11 19:11:02 +0530

    Time: 9 ms (43.20%) | Memory: 42.1 MB (33.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -19,11 +19,16 @@ class Solution {
 
         //optimal approach
 
-        if(n == 0){
-            return 0;
-        }
-        if(n == 1){
-            return 1;
+        // if(n == 0){
+        //     return 0;
+        // }
+        // if(n == 1){
+        //     return 1;
+        // }
+        
+        //or
+        if(n < 2){
+            return n;
         }
         
         return fib(n - 1) + fib(n - 2);
