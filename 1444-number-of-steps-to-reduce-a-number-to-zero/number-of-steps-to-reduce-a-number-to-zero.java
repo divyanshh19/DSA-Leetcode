9c8c211378014f9fd959aeee6572884b1c72e5c8
Divyansh Dubey <167629510+divyanshh19@users.noreply.github.com>
2026-08-13 01:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (19.03%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,19 +1,30 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int steps = 0;
+        //iterative approach
+        // int steps = 0;
+        // if(num == 0){
+        //     return 0;
+        // }
+        // while(num != 0){
+        //     if(num % 2 == 0){ //even 
+        //         num = num/2;
+        //         steps++;
+        //     }
+        //     if(num % 2 != 0){ //odd
+        //         num = num - 1;
+        //         steps++;
+        //     }
+        // }
+        // return steps;
+            return helper(num , 0);
+    }
+    public static int helper(int num , int steps){
         if(num == 0){
-            return 0;
+            return steps;
         }
-        while(num != 0){
-            if(num % 2 == 0){ //even 
-                num = num/2;
-                steps++;
-            }
-            if(num % 2 != 0){ //odd
-                num = num - 1;
-                steps++;
-            }
+        if(num % 2 == 0){
+            return helper(num/2 , steps+1);
         }
-        return steps;
+        return helper(num-1, steps+1);
     }
 }
