ac84e9438de4fd9e6b6e42c2efe19898a9eea05c
Divyansh Dubey <167629510+divyanshh19@users.noreply.github.com>
2026-08-30 15:02:40 +0530

    Time: 2 ms (34.78%) | Memory: 45.9 MB (9.84%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -27,31 +27,55 @@ class Solution {
     //     return ans;
     //Recursion -
 
+    // List<List<Integer>> ans = new ArrayList<>();
+    // List<Integer> ds = new ArrayList<>();
+    // boolean[] freq = new boolean[nums.length];
+    // recurPermute(nums , ds , ans, freq);
+    // return ans;
+    // }
+    // public void recurPermute(int[] nums , List<Integer> ds , List<List<Integer>> ans , boolean[] freq){
+    //     if(ds.size() == nums.length){
+    //         ans.add(new ArrayList<>(ds));
+    //         return;
+    //     }
+
+    //     for(int i = 0; i < nums.length;i++){
+    //         if(!freq[i]){
+    //             //first make the true in boolean array for adding an element
+    //             freq[i] = true;
+    //             //add the element
+    //             ds.add(nums[i]);
+    //             //explore the others
+    //             recurPermute(nums , ds , ans, freq);
+    //             //backtrack
+    //             ds.remove(ds.size() - 1);
+    //             //make the false in boolean array for current element
+    //             freq[i] = false;
+    //         }
+    //     }
+    //Swapping 
     List<List<Integer>> ans = new ArrayList<>();
-    List<Integer> ds = new ArrayList<>();
-    boolean[] freq = new boolean[nums.length];
-    recurPermute(nums , ds , ans, freq);
+    recurPermute(0 , ans ,nums );
     return ans;
     }
-    public void recurPermute(int[] nums , List<Integer> ds , List<List<Integer>> ans , boolean[] freq){
-        if(ds.size() == nums.length){
+    private void recurPermute(int ind ,List<List<Integer>> ans, int[] nums ){
+        if(ind == nums.length){
+            List<Integer> ds = new ArrayList<>();
+            for(int i = 0;i < nums.length;i++){
+                ds.add(nums[i]);
+            }
             ans.add(new ArrayList<>(ds));
             return;
         }
-
-        for(int i = 0; i < nums.length;i++){
-            if(!freq[i]){
-                //first make the true in boolean array for adding an element
-                freq[i] = true;
-                //add the element
-                ds.add(nums[i]);
-                //explore the others
-                recurPermute(nums , ds , ans, freq);
-                //backtrack
-                ds.remove(ds.size() - 1);
-                //make the false in boolean array for current element
-                freq[i] = false;
-            }
+        for(int i = ind; i < nums.length;i++){
+            swap(i , ind ,nums);
+            recurPermute(ind + 1, ans, nums );
+            swap(i , ind ,nums);
         }
+    }
+    private void swap(int i ,int ind ,int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[ind];
+        nums[ind] = temp;
     }
 }
