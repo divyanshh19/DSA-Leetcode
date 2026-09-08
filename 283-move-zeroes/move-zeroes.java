6e87af31ab9b3ba95460d3c1e71e204dbae3e6c5
Divyansh Dubey <167629510+divyanshh19@users.noreply.github.com>
2026-09-08 13:24:17 +0530

    Time: 1 ms (99.89%) | Memory: 47.8 MB (38.25%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -25,14 +25,23 @@ class Solution {
     //         left++;
     //     }
     // }
-        int j = 0;
-        for(int i = 0;i < nums.length;i++){
-            if(nums[i] != 0){
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                j++; 
+        // int j = 0;
+        // for(int i = 0;i < nums.length;i++){
+        //     if(nums[i] != 0){
+        //         int temp = nums[j];
+        //         nums[j] = nums[i];
+        //         nums[i] = temp;
+        //         j++; 
+        //     }
+        // }
+        int index = 0;
+        for(int num : nums){
+            if(num != 0){
+                nums[index++] = num;
             }
+        }
+        while(index < nums.length){
+            nums[index++] = 0;
         }
     }
 }
