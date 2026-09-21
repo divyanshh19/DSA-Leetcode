d74fc4f25223bbbc6dfcbb798a4a987f4a9484ec
Divyansh Dubey <167629510+divyanshh19@users.noreply.github.com>
2026-09-21 13:58:24 +0530

    Time: 3 ms (86.03%) | Memory: 43.4 MB (41.02%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,19 +5,20 @@ class Solution {
         for(char str : s.toCharArray()){
             if(str == '(' || str == '[' || str == '{'){
                 st.push(str);
-                continue;
-            }
-            if(st.isEmpty()){
-                return false;
-            }
-
-            char top = st.peek();
-
-            if(str == ')' && top == '('  || str == ']' && top == '['  || str == '}' && top == '{'){
-                st.pop();
             }
             else{
+                if(st.isEmpty()){
                 return false;
+                }
+
+                char top = st.peek();
+
+                if(str == ')' && top == '('  || str == ']' && top == '['  || str == '}' && top == '{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
             }
         }
         return st.isEmpty();
