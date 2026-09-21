876f65e1e9bdbdcf9b6e759cba78cdfbad0bd902
Divyansh Dubey <167629510+divyanshh19@users.noreply.github.com>
2026-09-21 13:54:59 +0530

    Time: 3 ms (86.03%) | Memory: 43.1 MB (85.47%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,42 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        // Stack<Character> st = new Stack<>();
-        // for(char c : s.toCharArray()){
-        //     if(c == '(' || c == '[' || c == '{'){
-        //         st.push(c);
-        //     }
-        //     else{
-        //         if(st.isEmpty()){
-        //             return false;
-        //         }
-        //         char top = st.peek();
-        //         if(c == ')' && top == '(' || c == ']' && top == '[' || c == '}' && top == '{'){
-        //             st.pop();
-        //         }
-        //         else{
-        //             return false;
-        //         }
-        //     }
-        // }
-        // return st.isEmpty(); // return true if all the parantheses are matched and popped out
         Stack<Character> st = new Stack<>();
-        for(char t : s.toCharArray()){
-            if(t == '(' || t == '[' || t == '{'){
-                st.push(t);  //sare opening bracket stack mein
+
+        for(char str : s.toCharArray()){
+            if(str == '(' || str == '[' || str == '{'){
+                st.push(str);
+                continue;
+            }
+            if(st.isEmpty()){
+                return false;
+            }
+
+            char top = st.peek();
+
+            if(str == ')' && top == '('  || str == ']' && top == '['  || str == '}' && top == '{'){
+                st.pop();
             }
             else{
-                if(st.isEmpty()){
-                    return false;
-                }
-                char top = st.peek();
-                if(t == ')' && top == '(' || t == ']' && top == '[' || t == '}' && top == '{'){
-                    st.pop(); //compare bracket with stack bracket is same then pop it
-                }
-                else {
-                    return false;
-                }
+                return false;
             }
         }
-        return st.isEmpty(); //if stack is empty it's true
+        return st.isEmpty();
     }
 }
