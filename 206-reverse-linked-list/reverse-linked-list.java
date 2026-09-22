c6485391f5b90f267fd6fb16a659ae8eaec7dd9f
Divyansh Dubey <167629510+divyanshh19@users.noreply.github.com>
2026-09-23 00:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (30.87%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -24,15 +24,15 @@ class Solution {
 
         //Recursive approach --
 
+        
         if(head == null || head.next == null){
             return head;
         }
-        ListNode newHead = reverseList(head.next);
+        ListNode newNode = reverseList(head.next);
 
         ListNode front = head.next;
-        front.next = head;
+        front.next = head;;
         head.next = null;
-
-        return newHead;
+        return newNode;
     }
 }
